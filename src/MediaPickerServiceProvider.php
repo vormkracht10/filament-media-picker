245f35ca1bffae57bd2eae68f9bbd2845dd935d0
Baspa <Baspa@users.noreply.github.com>
2024-11-28 10:05:36 +0000

    Fix styling

--- src/MediaPickerServiceProvider.php
+++ src/MediaPickerServiceProvider.php
@@ -9,7 +9,6 @@ use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
-use Illuminate\Support\Facades\Config;
 use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
