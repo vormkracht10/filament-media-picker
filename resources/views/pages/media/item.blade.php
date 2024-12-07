ffa283c259e238720b2da071aecff1b09f67c563
Baspa <hello@baspa.dev>
2024-12-07 12:46:06 +0100

    wip

--- resources/views/pages/media/item.blade.php
+++ resources/views/pages/media/item.blade.php
@@ -3,9 +3,7 @@
     <button type="button" wire:click="mountAction('showMedia', { ulid: '{{ $media->ulid }}' })"
         class="group block w-full text-left focus:outline-none">
         <div class="block w-full overflow-hidden rounded-lg bg-gray-100 ring-2 ring-indigo-500 ring-offset-2">
-            <div class="aspect-square overflow-hidden flex items-center justify-center">
-                <img src="{{ $media->src }}" alt="" class="max-h-full max-w-full object-cover object-center">
-            </div>
+            <img src="{{ $media->src }}" alt="" class="pointer-events-none aspect-[10/7] object-cover">
         </div>
         <p class="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
             {{ $media->filename }}
