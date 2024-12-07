d1112051877a6981b3712d9cfa98017130fd91fd
Baspa <hello@baspa.dev>
2024-12-07 15:30:03 +0100

    Fix styling

--- resources/views/pages/media/item.blade.php
+++ resources/views/pages/media/item.blade.php
@@ -2,7 +2,7 @@
 <li class="relative">
     <button type="button" wire:click="mountAction('showMedia', { ulid: '{{ $media->ulid }}' })"
         class="group block w-full text-left focus:outline-none">
-        <div class="block w-full overflow-hidden rounded-lg bg-gray-100 ring-2 ring-indigo-500 ring-offset-2">
+        <div class="block w-full overflow-hidden rounded-lg bg-gray-100 ring-offset-2">
             <img src="{{ $media->src }}" alt="" class="pointer-events-none aspect-[10/7] object-cover">
         </div>
         <p class="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
