1b07ff38b69f3c8a7448f82e966b411a2ab835ca
Baspa <hello@baspa.dev>
2024-12-07 15:49:34 +0100

    Set active ring

--- resources/views/pages/media/item.blade.php
+++ resources/views/pages/media/item.blade.php
@@ -1,8 +1,9 @@
 @props(['media'])
-<li class="relative">
+<li class="relative" :class="{ 'ring-2 ring-primary-500 ring-offset-2': selectedMedia === '{{ $media->ulid }}' }">
     <button type="button" wire:click="mountAction('showMedia', { ulid: '{{ $media->ulid }}' })"
+        @click="selectedMedia = selectedMedia === '{{ $media->ulid }}' ? null : '{{ $media->ulid }}'"
         class="group block w-full text-left focus:outline-none">
-        <div class="block w-full overflow-hidden rounded-lg bg-gray-100 ring-offset-2">
+        <div class="block w-full overflow-hidden rounded-lg bg-gray-100">
             <img src="{{ $media->src }}" alt="" class="pointer-events-none aspect-[10/7] object-cover">
         </div>
         <p class="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
--- resources/views/pages/media/library.blade.php
+++ resources/views/pages/media/library.blade.php
@@ -2,7 +2,7 @@
     <div class="flex flex-row relative">
         <div class="mr-2 flex-grow">
             <section class="mt-8 pb-16" aria-labelledby="gallery-heading">
-                <ul role="list"
+                <ul role="list" x-data="{ selectedMedia: null }"
                     class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                     @foreach ($media as $m)
                         @include('filament-media-picker::pages.media.item', ['media' => $m])
