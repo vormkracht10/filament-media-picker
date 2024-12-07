1bf391ea5e8e52a4df64ff3e054844715e47fc69
Baspa <Baspa@users.noreply.github.com>
2024-12-07 10:54:26 +0000

    Fix styling

--- src/Resources/MediaResource.php
+++ src/Resources/MediaResource.php
@@ -4,9 +4,7 @@ namespace Vormkracht10\MediaPicker\Resources;
 
 use Filament\Facades\Filament;
 use Filament\Forms\Components\Section;
-use Filament\Forms\Components\Select;
 use Filament\Forms\Form;
-use Filament\Forms\Get;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\IconColumn;
