570bd267555394443ab567444afc807a186b6a09
Baspa <hello@baspa.dev>
2024-12-07 11:10:55 +0100

    wip

--- src/MediaPicker.php
+++ src/MediaPicker.php
@@ -2,4 +2,68 @@
 
 namespace Vormkracht10\MediaPicker;
 
-class MediaPicker {}
+use Filament\Facades\Filament;
+use Illuminate\Support\Facades\Storage;
+use Vormkracht10\MediaPicker\Models\Media;
+
+class MediaPicker
+{
+    public static function create(array $data): array
+    {
+        $media = [];
+
+        foreach ($data['media'] as $file) {
+            // Get the full path on the configured disk
+            $fullPath = Storage::disk(config('media-picker.disk'))->path($file);
+
+            $filename = basename($file);
+
+            $mimeType = Storage::disk(config('media-picker.disk'))->mimeType($file);
+
+            $fileSize = Storage::disk(config('media-picker.disk'))->size($file);
+
+            $extension = pathinfo($filename, PATHINFO_EXTENSION);
+
+            // Additional file information
+            $fileInfo = [
+                'full_path' => $fullPath,
+                'filename' => $filename,
+                'extension' => $extension,
+                'mime_type' => $mimeType,
+                'file_size' => $fileSize,
+            ];
+
+            if (str_starts_with($mimeType, 'image/')) {
+                try {
+                    $imageSize = getimagesize($fullPath);
+                    $fileInfo += [
+                        'width' => $imageSize[0] ?? null,
+                        'height' => $imageSize[1] ?? null,
+                        'image_type' => $imageSize[2] ?? null,
+                    ];
+                } catch (\Exception $e) {
+                    // Log or handle image size extraction error
+                }
+            }
+
+            $media[] = Media::create([
+                'site_ulid' => Filament::getTenant()->ulid,
+                'model_type' => $data['model_type'] ?? null,
+                'model_id' => $data['model_id'] ?? null,
+                'disk' => config('media-picker.disk'),
+                'uploaded_by' => auth()->id(),
+                'filename' => $filename,
+                'extension' => $extension,
+                'mime_type' => $mimeType,
+                'size' => $fileSize,
+                'width' => $fileInfo['width'] ?? null,
+                'height' => $fileInfo['height'] ?? null,
+                'checksum' => md5_file($fullPath),
+                'public' => config('media-picker.visibility') === 'public', // TODO: Should be configurable in the form itself
+                'position' => Media::max('position') + 1, // TODO: Check if this is correct
+            ]);
+        }
+
+        return $media;
+    }
+}
