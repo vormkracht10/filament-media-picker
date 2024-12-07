e9538aab1bd9ccc9e40621e103a06b582d89d1e4
Baspa <hello@baspa.dev>
2024-12-07 12:37:23 +0100

    Equal size images

--- resources/views/pages/media/item.blade.php
+++ resources/views/pages/media/item.blade.php
@@ -3,7 +3,9 @@
     <button type="button" wire:click="mountAction('showMedia', { ulid: '{{ $media->ulid }}' })"
         class="group block w-full text-left focus:outline-none">
         <div class="block w-full overflow-hidden rounded-lg bg-gray-100 ring-2 ring-indigo-500 ring-offset-2">
-            <img src="{{ $media->src }}" alt="" class="pointer-events-none aspect-[10/7] object-cover">
+            <div class="aspect-square overflow-hidden flex items-center justify-center">
+                <img src="{{ $media->src }}" alt="" class="w-full h-full object-cover object-center">
+            </div>
         </div>
         <p class="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
             {{ $media->filename }}
